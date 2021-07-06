80c0260cb30591df9479e07470c016e325e80530
lamnguyenxuan <nguyenxuanlam.nguyen2000@gmail.com>
2021-07-06 20:47:36 +0700

    Excersise

---
+++ 05OnTap/dataanalysis/src/main/java/vn/techmaster/dataanalysis/model/Person.java
@@ -0,0 +1,39 @@
+package vn.techmaster.dataanalysis.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+  private String name;
+  private String nationality;
+  private int age;
+  public Person(String name, String nationality, int age) {
+    this.name = name;
+    this.nationality = nationality;
+    this.age = age;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public String getNationality() {
+    return nationality;
+  }
+  public void setNationality(String nationality) {
+    this.nationality = nationality;
+  }
+  public int getAge() {
+    return age;
+  }
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  
+}
---
+++ 05OnTap/stream/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) {
+      // DemoStream.groupBy01();
+      DemoStream.findAge();
+      System.out.println();
+      DemoStream.aveAge();
+      System.out.println();
+      DemoStream.aveAgeByNationality();
+    }
+  }
